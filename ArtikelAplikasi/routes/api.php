ad0bef9e0d3de8c4a638afe57a0006dec2bf98b1
iladsuma <67311820+iladsuma@users.noreply.github.com>
2024-04-24 05:13:38 +0700

    add login logic

--- ArtikelAplikasi/routes/api.php
+++ ArtikelAplikasi/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+ });
+ Route::post('/login', [LoginController::class, 'index']);
+ Route::get('/logout', [LoginController::class, 'logout']);
